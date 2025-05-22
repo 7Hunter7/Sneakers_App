63f16e3704d7e8c39ca117e5e0bbac293a2c6d6b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-22 21:52:36 +0300

    upd: added HelloComponent component and using it in App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-
+import HelloComponent from './components/HelloComponent.vue';
 </script>
 
 <template>
-
-
+  <hello-component></hello-component>
 </template>
 
 <style scoped></style>
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello</h1>
+  <p>Welcome to the Sneakers App!</p>
+  <button @click="handleClick">Click Me</button>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello, Sneakers App!');
+
+function handleClick() {
+  alert(message.value);
+}
+
+</script>
