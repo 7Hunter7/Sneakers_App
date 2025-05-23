6ec8938eb1ea0c89b432c116eb2e0d265116dfb8
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-23 07:05:18 +0300

    upd: Added tailwindcss plugin to Vite

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import HelloComponent from './components/HelloComponent.vue';
 </script>
 
 <template>
-  <hello-component></hello-component>
+  <div class="container mx-auto mb-40 flex justify-center items-center flex-col">
+    <hello-component></hello-component>
+  </div>
 </template>
 
 <style scoped></style>
