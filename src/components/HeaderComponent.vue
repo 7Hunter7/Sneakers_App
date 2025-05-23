78fb4e80dc42342d5deb5104440553f612f83b46
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-23 07:50:01 +0300

    upd: added styles for header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,25 +1,25 @@
 <template>
-  <header>
-    <div>
-      <img src="/logo.png" alt="Logo">
+  <header class="flex justify-between border-b border-slate-300 px-8 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-12">
       <div>
-        <h2>Best Sneakers</h2>
-        <p>Магазин лучших кроссовок</p>
+        <h2 class="text-xl font-bold uppercase">Best Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
       </div>
     </div>
 
-    <ul>
-      <li>
+    <ul class="flex items-center gap-8">
+      <li class="flex items-center gap-3 text-gray-500">
         <img src="/cart.svg" alt="Cart">
         <b>Стоимость</b>
       </li>
-      <li>
+      <li class="flex items-center gap-3 text-gray-500">
         <img src="/heart.svg" alt="Favorites">
-        <b>Закладки</b>
+        <span>Закладки</span>
       </li>
-      <li>
+      <li class="flex items-center gap-3 text-gray-500">
         <img src="/profile.svg" alt="Profile">
-        <b>Профиль</b>
+        <span>Профиль</span>
       </li>
     </ul>
   </header>
