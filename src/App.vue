234a3fb188a8ddff89a43f1d4645ff0deb10937a
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-23 07:38:51 +0300

    upd:  updated Hello to HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloComponent from './components/HelloComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
     <h1 class="text-3xl text-center font-bold my-5">Best Sneakers</h1>
-    <HelloComponent />
+    <HeaderComponent />
   </div>
 </template>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div>
+      <img src="/logo.png" alt="Logo">
+      <div>
+        <h2>Best Sneakers</h2>
+        <p>Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul>
+      <li>
+        <img src="/cart.svg" alt="Cart">
+        <b>Стоимость</b>
+      </li>
+      <li>
+        <img src="/heart.svg" alt="Favorites">
+        <b>Закладки</b>
+      </li>
+      <li>
+        <img src="/profile.svg" alt="Profile">
+        <b>Профиль</b>
+      </li>
+    </ul>
+  </header>
+</template>
+
+
+<script setup>
+</script>
