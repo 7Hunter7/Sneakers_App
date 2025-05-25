5ec0ffe73021aaf608745d57e4fa1b8e8b7c72ac
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 19:20:17 +0300

    upd: created new component CartItem

---
+++ src/components/CartItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center gap-4 bg-white p-4 border border-slate-200 rounded-xl">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneakers">
+    <div class="flex flex-col">
+      <p class="">Кроссовки Nike Blazer Mid Suede</p>
+      <div class="flex justify-between mt-2">
+        <b>1205 руб.</b>
+        <svg class="cursor-pointer transition opacity-40  hover:opacity-100" width="32" height="32" viewBox="0 0 32 32"
+          fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
+            fill="#B5B5B5" />
+        </svg>
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/DrawerComponent.vue
+++ src/components/DrawerComponent.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="fixed top-0 right-0 w-full h-full bg-black/70 z-10">
-    <DrawerHead />
+  <div class="fixed top-0 left-0 w-full h-full bg-black/70 z-10">
+    <div class="fixed top-0 right-0 w-96 h-full bg-white z-20 p-8">
+      <DrawerHead />
+      <CartItem />
+    </div>
   </div>
 </template>
 
 <script setup>
+import CartItem from './CartItem.vue';
 import DrawerHead from './DrawerHead.vue';
 </script>
