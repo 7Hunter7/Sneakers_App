764f1d11b617dc44c1ac7386defb6d9fccf0c4a1
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 18:28:58 +0300

    upd: added using props: onClickAdd and onClickFavorite in Cart

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <div
     class="relative bg-white p-8 border border-slate-100 rounded-3xl cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
-    <img :src="!isFavorite ? '/like-1.svg' : 'like-2.svg'" alt="Like" class="absolute top-2 left-2">
+    <img :src="!isFavorite ? '/like-1.svg' : 'like-2.svg'" alt="Like" class="absolute top-2 left-2"
+      @click="onClickFavorite">
     <img :src="imageUrl" alt="Sneakers">
     <h2 class="mt-2">{{ title }}</h2>
     <div class="flex justify-between mt-5">
@@ -9,7 +10,8 @@
         <span class="text-slate-400 uppercase">Цена:</span>
         <b>{{ price }} руб.</b>
       </div>
-      <img :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus">
+      <img :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" @click="onClickAdd"
+        class="cursor-pointer hover:scale-110 transition">
     </div>
   </div>
 </template>
@@ -20,6 +22,8 @@ defineProps({
   title: String,
   price: Number,
   isFavorite: Boolean,
-  isAdded: Boolean
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
 });
 </script>
--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
     <CartComponent imageUrl="/sneakers/sneakers-1.jpg" title="Мужские Кроссовки Nike Blazer Mid Suede" :price="1205"
-      isAdded isFavorite />
+      isAdded isFavorite :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" />
     <CartComponent imageUrl="/sneakers/sneakers-2.jpg" />
     <CartComponent imageUrl="/sneakers/sneakers-3.jpg" />
     <CartComponent imageUrl="/sneakers/sneakers-4.jpg" />
@@ -12,4 +12,12 @@
 
 <script setup>
 import CartComponent from './CartComponent.vue';
+
+const onClickAdd = () => {
+  console.log('Add to cart');
+};
+
+const onClickFavorite = () => {
+  console.log('Add to favorites');
+};
 </script>
