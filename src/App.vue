e71a5e11c50e87e1e417d353e9ec3d36692636f4
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 09:09:11 +0300

    upd: updated using CartComponent in App

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,14 @@ import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <h1 class="text-3xl text-center font-bold my-5">Best Sneakers</h1>
     <HeaderComponent />
+    <CartComponent />
+    <CartComponent />
+    <CartComponent />
+    <CartComponent />
+    <CartComponent />
     <CartComponent />
   </div>
 </template>
