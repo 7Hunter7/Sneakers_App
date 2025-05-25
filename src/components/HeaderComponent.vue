3f0cce8a598bd79e219f2fea41d1404bb2b2e13c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 09:10:41 +0300

    upd: updated border HeaderComponen

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex justify-between border-b border-slate-300 px-8 py-8">
+  <header class="flex justify-between border-b border-slate-200 px-8 py-8">
     <div class="flex items-center gap-4">
       <img src="/logo.png" alt="Logo" class="w-12">
       <div>
