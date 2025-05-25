4b7ab7b513d3bb34417f4b29ccef9015c430937f
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 18:50:57 +0300

    upd: created new component DrawerComponent

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import CartList from './components/CartList.vue';
+import DrawerComponent from './components/DrawerComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
+  <DrawerComponent />
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <HeaderComponent />
 
