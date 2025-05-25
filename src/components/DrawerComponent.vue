919801104c76fdb117bfb30d040678198427ea2e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 19:30:34 +0300

    upd: created new component CartItemList

--- src/components/DrawerComponent.vue
+++ src/components/DrawerComponent.vue
@@ -2,15 +2,12 @@
   <div class="fixed top-0 left-0 w-full h-full bg-black/70 z-10">
     <div class="fixed top-0 right-0 w-96 h-full bg-white z-20 p-8">
       <DrawerHead />
-      <CartItem />
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      <CartItemList />
     </div>
   </div>
 </template>
 
 <script setup>
-import CartItem from './CartItem.vue';
 import DrawerHead from './DrawerHead.vue';
+import CartItemList from './CartItemList.vue';
 </script>
