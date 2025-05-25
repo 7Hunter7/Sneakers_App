e53552bbaa6016cb56553459525477c9bba32d8a
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 09:28:22 +0300

    upd: added using props in Cart

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -2,7 +2,7 @@
   <div
     class="relative bg-white p-8 border border-slate-100 rounded-3xl cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
     <img src="/like-2.svg" alt="Like" class="absolute top-2 left-2">
-    <img src="/sneakers/sneakers-1.jpg" alt="Sneakers">
+    <img :src="imageUrl" alt="Sneakers">
     <h2 class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</h2>
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
@@ -13,3 +13,9 @@
     </div>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  imageUrl: String
+});
+</script>
--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <CartComponent />
-    <CartComponent />
-    <CartComponent />
-    <CartComponent />
-    <CartComponent />
-    <CartComponent />
+    <CartComponent imageUrl="/sneakers/sneakers-1.jpg" />
+    <CartComponent imageUrl="/sneakers/sneakers-2.jpg" />
+    <CartComponent imageUrl="/sneakers/sneakers-3.jpg" />
+    <CartComponent imageUrl="/sneakers/sneakers-4.jpg" />
+    <CartComponent imageUrl="/sneakers/sneakers-5.jpg" />
+    <CartComponent imageUrl="/sneakers/sneakers-6.jpg" />
   </div>
 </template>
 
