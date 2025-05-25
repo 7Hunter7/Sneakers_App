6104eea0433feae335025588aabe37171d18e8c7
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 19:22:27 +0300

    upd: updated styles DrawerHead

--- src/components/DrawerComponent.vue
+++ src/components/DrawerComponent.vue
@@ -3,6 +3,9 @@
     <div class="fixed top-0 right-0 w-96 h-full bg-white z-20 p-8">
       <DrawerHead />
       <CartItem />
+      <CartItem />
+      <CartItem />
+      <CartItem />
     </div>
   </div>
 </template>
