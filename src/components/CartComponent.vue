86940e4ee248bee0e668a4778c0dde031e081883
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-25 09:35:37 +0300

    upd: added using props: title and price in Cart

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -3,11 +3,11 @@
     class="relative bg-white p-8 border border-slate-100 rounded-3xl cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
     <img src="/like-2.svg" alt="Like" class="absolute top-2 left-2">
     <img :src="imageUrl" alt="Sneakers">
-    <h2 class="mt-2">Мужские Кроссовки Nike Blazer Mid Suede</h2>
+    <h2 class="mt-2">{{ title }}</h2>
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400 uppercase">Цена:</span>
-        <b>1205 руб.</b>
+        <b>{{ price }} руб.</b>
       </div>
       <img src="/plus.svg" alt="Plus">
     </div>
@@ -16,6 +16,8 @@
 
 <script setup>
 defineProps({
-  imageUrl: String
+  imageUrl: String,
+  title: String,
+  price: Number
 });
 </script>
--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <CartComponent imageUrl="/sneakers/sneakers-1.jpg" />
+    <CartComponent imageUrl="/sneakers/sneakers-1.jpg" title="Мужские Кроссовки Nike Blazer Mid Suede" :price="1205" />
     <CartComponent imageUrl="/sneakers/sneakers-2.jpg" />
     <CartComponent imageUrl="/sneakers/sneakers-3.jpg" />
     <CartComponent imageUrl="/sneakers/sneakers-4.jpg" />
     <CartComponent imageUrl="/sneakers/sneakers-5.jpg" />
     <CartComponent imageUrl="/sneakers/sneakers-6.jpg" />
-  </div>
+  </div>Мужские Кроссовки Nike Blazer Mid Suede
 </template>
 
 <script setup>
