adaa29402cb62aa4cc0011a2509aa8fbb9c831dd
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-26 22:05:42 +0300

    upd: updated App - added using onChangeSearchInput() updated fetchItems()

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HeaderComponent from './components/HeaderComponent.vue';
 const items = ref([]);
 
 const filters = reactive({
-  sortBy: '',
+  sortBy: 'title',
   searchQuery: ''
 });
 
@@ -17,18 +17,26 @@ const onChangeSelect = (event) => {
   filters.sortBy = event.target.value;
 };
 
+// Функция для обработки изменения поиска
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value;
+};
+
 // Функция для получения данных с сервера
 const fetchItems = async () => {
   try {
     const params = {
       sortBy: filters.sortBy,
-      searchQuery: filters.searchQuery
     };
 
-    const { data } = await axios.get(
-      `https://7c1179b9d2e1c831.mokky.dev/items?title=*${filters.searchQuery}*&sort=${filters.sortBy}`,
-    );
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`;
+    }
 
+    const { data } = await axios.get(
+      `https://7c1179b9d2e1c831.mokky.dev/items`,
+      { params }
+    );
     items.value = data;
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -55,12 +63,13 @@ watch(filters, fetchItems);
             class="border border-gray-200 rounded-lg py-2 px-3 w-40 outline-none focus:border-lime-400" name="filter"
             id="">
             <option value="name">По названию</option>
-            <option value="-price">По цене(сначала дешевые)</option>
-            <option value="price">По цене(сначала дорогие)</option>
+            <option value="price">По цене(сначала дешевые)</option>
+            <option value="-price">По цене(сначала дорогие)</option>
           </select>
           <div class="relative">
             <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
-            <input class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
+            <input @input="onChangeSearchInput"
+              class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
               type="text" placeholder="Поиск..." />
           </div>
         </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <CardComponent v-for="item in items" :key="item.id" :imageUrl="item.imageUrl" :title="item.name" :price="item.price"
-      :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" />
+    <CardComponent v-for="item in items" :key="item.id" :imageUrl="item.imageUrl" :title="item.title"
+      :price="item.price" :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" />
   </div>
 </template>
 
