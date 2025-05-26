e8c2fe931548aeaa8485e4727acc3295eb967331
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-26 17:35:27 +0300

    upd: added select to App

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,17 @@ import HeaderComponent from './components/HeaderComponent.vue';
     <HeaderComponent />
 
     <div class="p-10">
-      <div class="flex justify-between">
-        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center mb-8">
+        <h2 class="text-3xl font-bold">Все кроссовки</h2>
+        <select class="border border-gray-200 rounded-lg py-2 px-3 outline-none focus:border-lime-400" name="filter"
+          id="">
+          <option value="all">Все</option>
+          <option value="new">Новинки</option>
+          <option value="popular">Популярные</option>
+          <option value="sale">Распродажа</option>
+          <option value="cheap">Дешевые</option>
+          <option value="expensive">Дорогие</option>
+        </select>
         <div class="relative">
           <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
           <input class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
