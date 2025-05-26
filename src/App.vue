2a527ec55bbd6cd2856b0f91d1ee5218e334650e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-26 17:39:08 +0300

    upd: updated using  select and search in App

--- src/App.vue
+++ src/App.vue
@@ -12,19 +12,21 @@ import HeaderComponent from './components/HeaderComponent.vue';
     <div class="p-10">
       <div class="flex justify-between items-center mb-8">
         <h2 class="text-3xl font-bold">Все кроссовки</h2>
-        <select class="border border-gray-200 rounded-lg py-2 px-3 outline-none focus:border-lime-400" name="filter"
-          id="">
-          <option value="all">Все</option>
-          <option value="new">Новинки</option>
-          <option value="popular">Популярные</option>
-          <option value="sale">Распродажа</option>
-          <option value="cheap">Дешевые</option>
-          <option value="expensive">Дорогие</option>
-        </select>
-        <div class="relative">
-          <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
-          <input class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
-            type="text" placeholder="Поиск..." />
+        <div class="flex items-center gap-4">
+          <select class="border border-gray-200 rounded-lg py-2 px-3 w-40 outline-none focus:border-lime-400"
+            name="filter" id="">
+            <option value="all">Все</option>
+            <option value="new">Новинки</option>
+            <option value="popular">Популярные</option>
+            <option value="sale">Распродажа</option>
+            <option value="cheap">Дешевые</option>
+            <option value="expensive">Дорогие</option>
+          </select>
+          <div class="relative">
+            <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
+            <input class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
+              type="text" placeholder="Поиск..." />
+          </div>
         </div>
       </div>
       <CartList />
