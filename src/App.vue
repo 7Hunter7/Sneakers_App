7804bfc26391dce4db33390f8a67bacddc1a1450
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-26 17:30:07 +0300

    upd: added search to App

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@ import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
-  <DrawerComponent />
+  <!-- <DrawerComponent /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <HeaderComponent />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+        <div class="relative">
+          <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
+          <input class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
+            type="text" placeholder="Поиск..." />
+        </div>
+      </div>
       <CartList />
     </div>
   </div>
