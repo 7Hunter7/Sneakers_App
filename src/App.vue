a6a604aa108ef4962e7f5bbbb99cacf355af7d7f
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-26 18:07:41 +0300

    upd: updated CardList using props and 'v-for ' for CardComponent

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,45 @@
 import CardList from './components/CardList.vue';
 import DrawerComponent from './components/DrawerComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
+
+const items = [
+  {
+    "id": 1,
+    "name": "Мужские Кроссовки Nike Blazer Mid Suede",
+    "price": "1205",
+    "imageUrl": "/sneakers/sneakers-1.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Мужские Кроссовки Nike Air Max 270",
+    "price": "1390",
+    "imageUrl": "/sneakers/sneakers-2.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Мужские Кроссовки Adidas Yeezy Boost 350",
+    "price": "1790",
+    "imageUrl": "/sneakers/sneakers-3.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Мужские Кроссовки Puma RS-X",
+    "price": "1120",
+    "imageUrl": "/sneakers/sneakers-4.jpg"
+  },
+  {
+    "id": 5,
+    "name": "Мужские Кроссовки Reebok Classic",
+    "price": "980",
+    "imageUrl": "/sneakers/sneakers-5.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Мужские Кроссовки New Balance 574",
+    "price": "1245",
+    "imageUrl": "/sneakers/sneakers-6.jpg"
+  }
+]
 </script>
 
 <template>
@@ -29,7 +68,7 @@ import HeaderComponent from './components/HeaderComponent.vue';
           </div>
         </div>
       </div>
-      <CardList />
+      <CardList :items="items" />
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <CardComponent imageUrl="/sneakers/sneakers-1.jpg" title="Мужские Кроссовки Nike Blazer Mid Suede" :price="1205"
-      isAdded isFavorite :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" />
-    <CardComponent imageUrl="/sneakers/sneakers-2.jpg" />
-    <CardComponent imageUrl="/sneakers/sneakers-3.jpg" />
-    <CardComponent imageUrl="/sneakers/sneakers-4.jpg" />
-    <CardComponent imageUrl="/sneakers/sneakers-5.jpg" />
-    <CardComponent imageUrl="/sneakers/sneakers-6.jpg" />
-  </div>Мужские Кроссовки Nike Blazer Mid Suede
+    <CardComponent v-for="item in items" :key="item.id" :imageUrl="item.imageUrl" :title="item.name" :price="item.price"
+      :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" />
+  </div>
 </template>
 
 <script setup>
 import CardComponent from './CardComponent.vue';
+
+defineProps({
+  items: Array
+});
 
 const onClickAdd = () => {
   console.log('Add to cart');
