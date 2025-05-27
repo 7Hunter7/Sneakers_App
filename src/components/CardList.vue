144cc2063aff2f1ef2b80476f4679e93d53b623b
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-27 07:35:58 +0300

    upd(CardList): added using  isFavorite and isAdded to CardComponent

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="grid grid-cols-4 gap-5">
     <CardComponent v-for="item in items" :key="item.id" :imageUrl="item.imageUrl" :title="item.title"
-      :price="item.price" :onClickAdd="onClickAdd" :onClickFavorite="onClickFavorite" />
+      :price="item.price" :isFavorite="item.isFavorite" :isAdded="item.isAdded" :onClickAdd="onClickAdd"
+      :onClickFavorite="onClickFavorite" />
   </div>
 </template>
 
