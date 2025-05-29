d1b3048941a9f47a399c09dd511a75470d64a799
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-29 17:13:44 +0300

    upd(CardList): added using v-auto-animate

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-4 gap-5">
+  <div v-auto-animate class="grid grid-cols-4 gap-5">
     <CardComponent v-for="item in items" :key="item.id" :id="item.id" :imageUrl="item.imageUrl" :title="item.title"
       :price="item.price" :isFavorite="item.isFavorite" :isAdded="item.isAdded"
       :onClickAdd="() => emit('addToCart', item)" :onClickFavorite="() => emit('addToFavorite', item)" />
