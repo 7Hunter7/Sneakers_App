7b7437f1d278f345a024aab3dcfabac8c831f881
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-29 21:27:03 +0300

    upd(CardList): added using flag 'isFavorites'

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -2,7 +2,8 @@
   <div v-auto-animate class="grid grid-cols-4 gap-5">
     <CardComponent v-for="item in items" :key="item.id" :id="item.id" :imageUrl="item.imageUrl" :title="item.title"
       :price="item.price" :isFavorite="item.isFavorite" :isAdded="item.isAdded"
-      :onClickAdd="() => emit('addToCart', item)" :onClickFavorite="() => emit('addToFavorite', item)" />
+      :onClickAdd="isFavorites ? nunll : () => emit('addToCart', item)"
+      :onClickFavorite="isFavorites ? nunll : () => emit('addToFavorite', item)" />
   </div>
 </template>
 
@@ -10,7 +11,8 @@
 import CardComponent from './CardComponent.vue';
 
 defineProps({
-  items: Array
+  items: Array,
+  isFavorites: Boolean
 });
 
 const emit = defineEmits(['addToFavorite', 'addToCart']);
