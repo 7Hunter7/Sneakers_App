1e909ebc7698febe8e5a7509825df0c722fe1a78
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-29 18:34:19 +0300

    upd: added new page "HomePage" and using it in App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import axios from 'axios';
-import { computed, onMounted, provide, reactive, ref, watch } from 'vue';
-import CardList from './components/CardList.vue';
+import { computed, provide, ref, watch } from 'vue';
 import DrawerComponent from './components/DrawerComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
+import HomePage from './pages/HomePage.vue';
 
-const items = ref([]);
 const cartItems = ref([]);
 const drawerOpen = ref(false);
 const isCreatingOrder = ref(false);
@@ -27,94 +26,6 @@ const closeDrawer = () => {
   drawerOpen.value = false;
 };
 
-const filters = reactive({
-  sortBy: 'title',
-  searchQuery: ''
-});
-
-// Функция для обработки изменения сортировки
-const onChangeSelect = (event) => {
-  filters.sortBy = event.target.value;
-};
-
-// Функция для обработки изменения поиска
-const onChangeSearchInput = (event) => {
-  filters.searchQuery = event.target.value;
-};
-
-// Функция для получения данных с сервера
-const fetchItems = async () => {
-  try {
-    const params = {
-      sortBy: filters.sortBy,
-    };
-
-    if (filters.searchQuery) {
-      params.title = `*${filters.searchQuery}*`;
-    }
-
-    const { data } = await axios.get(
-      `https://7c1179b9d2e1c831.mokky.dev/items`,
-      { params }
-    );
-    items.value = data.map((item) => ({
-      ...item,
-      isAdded: false,
-      isFavorite: false,
-      favoriteId: null
-    }));
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
-
-// Функция для получения избранных товаров с сервера
-const fetchFavoriteItems = async () => {
-  try {
-    const { data: favorites } = await axios.get(
-      `https://7c1179b9d2e1c831.mokky.dev/favorites`,
-    );
-    items.value = items.value.map((item) => {
-      const favorite = favorites.find((fav) => fav.productId === item.id);
-      if (!favorite) {
-        return item;
-      }
-      return {
-        ...item,
-        isFavorite: true,
-        favoriteId: favorite.id,
-      };
-    });
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-};
-
-// Функция для добавления и удаления товара из избранного
-const addToFavorite = async (item) => {
-  try {
-    if (!item.isFavorite) {
-      const obj = {
-        productId: item.id,
-      };
-      item.isFavorite = true;
-      const { data } = await axios.post(
-        `https://7c1179b9d2e1c831.mokky.dev/favorites`,
-        obj
-      );
-      item.favoriteId = data.id;
-    } else {
-      item.isFavorite = false;
-      await axios.delete(
-        `https://7c1179b9d2e1c831.mokky.dev/favorites/${item.favoriteId}`
-      );
-      item.favoriteId = null;
-    }
-  } catch (error) {
-    console.error('Error adding to favorites:', error);
-  }
-};
-
 const addToCart = async (item) => {
   cartItems.value.push(item);
   item.isAdded = true;
@@ -123,15 +34,6 @@ const addToCart = async (item) => {
 const removeFromCart = async (item) => {
   cartItems.value.splice(cartItems.value.indexOf(item), 1);
   item.isAdded = false;
-};
-
-// Функция для добавления и удаления товара из корзины
-const onClickAddPlus = async (item) => {
-  if (!item.isAdded) {
-    addToCart(item);
-  } else {
-    removeFromCart(item);
-  }
 };
 
 // Функция для создания заказа
@@ -152,33 +54,6 @@ const createOrder = async () => {
     isCreatingOrder.value = false;
   }
 };
-
-// Загрузка данных при монтировании компонента
-onMounted(async () => {
-  // Проверка наличия корзины в localStorage
-  const savedCart = localStorage.getItem('cart');
-  if (savedCart) {
-    cartItems.value = JSON.parse(savedCart);
-  }
-  // Загрузка товаров и избранных товаров
-  await fetchItems();
-  await fetchFavoriteItems();
-  // Установка флага isAdded для каждого товара в зависимости от наличия в корзине
-  items.value = items.value.map((item) => ({
-    ...item,
-    isAdded: cartItems.value.some(cartItem => cartItem.id === item.id),
-  }));
-});
-// Реактивное отслеживание изменений в фильтрах
-watch(filters, fetchItems);
-
-// Реактивное отслеживание изменений корзины
-watch(cartItems, () => {
-  items.value = items.value.map((item) => ({
-    ...item,
-    isAdded: false,
-  }));
-})
 
 // Сохранение корзины в localStorage при изменении
 watch(cartItems, () => {
@@ -204,25 +79,7 @@ provide('cart', {
     <HeaderComponent @open-drawer="openDrawer" :total-price="totalPrice" />
 
     <div class="p-10">
-      <div class="flex justify-between items-center mb-8">
-        <h2 class="text-3xl font-bold">Все кроссовки</h2>
-        <div class="flex items-center gap-4">
-          <select @change="onChangeSelect"
-            class="border border-gray-200 rounded-lg py-2 px-3 w-40 outline-none focus:border-lime-400" name="filter"
-            id="">
-            <option value="name">По названию</option>
-            <option value="price">По цене(сначала дешевые)</option>
-            <option value="-price">По цене(сначала дорогие)</option>
-          </select>
-          <div class="relative">
-            <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
-            <input @input="onChangeSearchInput"
-              class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
-              type="text" placeholder="Поиск..." />
-          </div>
-        </div>
-      </div>
-      <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus" />
+      <HomePage />
     </div>
   </div>
 </template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="flex justify-between items-center mb-8">
+    <h2 class="text-3xl font-bold">Все кроссовки</h2>
+    <div class="flex items-center gap-4">
+      <select @change="onChangeSelect"
+        class="border border-gray-200 rounded-lg py-2 px-3 w-40 outline-none focus:border-lime-400" name="filter" id="">
+        <option value="name">По названию</option>
+        <option value="price">По цене(сначала дешевые)</option>
+        <option value="-price">По цене(сначала дорогие)</option>
+      </select>
+      <div class="relative">
+        <img class="absolute top-2.5 left-3" src="/search.svg" alt="Поиск">
+        <input @input="onChangeSearchInput"
+          class="border border-gray-200 rounded-lg py-1.5 w-64 pl-10 pr-4 outline-none focus:border-lime-400"
+          type="text" placeholder="Поиск..." />
+      </div>
+    </div>
+  </div>
+  <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus" />
+</template>
+
+<script setup>
+import axios from 'axios';
+import { onMounted, ref, reactive, inject, watch } from "vue";
+import CardList from '../components/CardList.vue';
+
+const { cartItems, addToCart, removeFromCart } = inject("cart");
+
+const items = ref([]);
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+});
+
+
+// Функция для получения данных с сервера
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy,
+    };
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`;
+    }
+
+    const { data } = await axios.get(
+      `https://7c1179b9d2e1c831.mokky.dev/items`,
+      { params }
+    );
+    items.value = data.map((item) => ({
+      ...item,
+      isAdded: false,
+      isFavorite: false,
+      favoriteId: null
+    }));
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+// Функция для получения избранных товаров с сервера
+const fetchFavoriteItems = async () => {
+  try {
+    const { data: favorites } = await axios.get(
+      `https://7c1179b9d2e1c831.mokky.dev/favorites`,
+    );
+    items.value = items.value.map((item) => {
+      const favorite = favorites.find((fav) => fav.productId === item.id);
+      if (!favorite) {
+        return item;
+      }
+      return {
+        ...item,
+        isFavorite: true,
+        favoriteId: favorite.id,
+      };
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+// Функция для обработки изменения сортировки
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value;
+};
+
+// Функция для обработки изменения поиска
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value;
+};
+
+// Функция для добавления и удаления товара из корзины
+const onClickAddPlus = async (item) => {
+  if (!item.isAdded) {
+    addToCart(item);
+  } else {
+    removeFromCart(item);
+  }
+};
+
+// Функция для добавления и удаления товара из избранного
+const addToFavorite = async (item) => {
+  try {
+    if (!item.isFavorite) {
+      const obj = {
+        productId: item.id,
+      };
+      item.isFavorite = true;
+      const { data } = await axios.post(
+        `https://7c1179b9d2e1c831.mokky.dev/favorites`,
+        obj
+      );
+      item.favoriteId = data.id;
+    } else {
+      item.isFavorite = false;
+      await axios.delete(
+        `https://7c1179b9d2e1c831.mokky.dev/favorites/${item.favoriteId}`
+      );
+      item.favoriteId = null;
+    }
+  } catch (error) {
+    console.error('Error adding to favorites:', error);
+  }
+};
+
+// Загрузка данных при монтировании компонента
+onMounted(async () => {
+  // Проверка наличия корзины в localStorage
+  const savedCart = localStorage.getItem('cart');
+  if (savedCart) {
+    cartItems.value = JSON.parse(savedCart);
+  }
+  // Загрузка товаров и избранных товаров
+  await fetchItems();
+  await fetchFavoriteItems();
+  // Установка флага isAdded для каждого товара в зависимости от наличия в корзине
+  items.value = items.value.map((item) => ({
+    ...item,
+    isAdded: cartItems.value.some(cartItem => cartItem.id === item.id),
+  }));
+});
+
+// Реактивное отслеживание изменений в фильтрах
+watch(filters, fetchItems);
+
+// Реактивное отслеживание изменений корзины
+watch(cartItems, () => {
+  items.value = items.value.map((item) => ({
+    ...item,
+    isAdded: false,
+  }));
+})
+</script>
