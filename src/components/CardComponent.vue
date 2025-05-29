1153639d1e5924ee48fd9206ad293d63d94e5959
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-29 21:33:01 +0300

    upd(Card): added using 'v-if' for onClickAdd and onClickFavorite imgs

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     class="relative bg-white p-8 border border-slate-100 rounded-3xl cursor-pointer hover:-translate-y-2 hover:shadow-xl transition">
-    <img :src="!isFavorite ? '/like-1.svg' : 'like-2.svg'" alt="Like" class="absolute top-2 left-2"
-      @click="onClickFavorite">
+    <img v-if="onClickFavorite" :src="!isFavorite ? '/like-1.svg' : 'like-2.svg'" alt="Like"
+      class="absolute top-2 left-2" @click="onClickFavorite">
     <img :src="imageUrl" alt="Sneakers">
     <h2 class="mt-2">{{ title }}</h2>
     <div class="flex justify-between mt-5">
@@ -10,7 +10,7 @@
         <span class="text-slate-400 uppercase">Цена:</span>
         <b>{{ price }} руб.</b>
       </div>
-      <img :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" @click="onClickAdd"
+      <img v-if="onClickAdd" :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" @click="onClickAdd"
         class="cursor-pointer hover:scale-110 transition">
     </div>
   </div>
