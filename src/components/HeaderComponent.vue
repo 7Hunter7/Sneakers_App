582948258db973ace33040538a6d6a8dd44e2b5d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-29 18:53:29 +0300

    upd(Header): added using RouterLink

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,12 +1,14 @@
 <template>
   <header class="flex justify-between border-b border-slate-200 px-8 py-8">
-    <div class="flex items-center gap-4">
-      <img src="/logo.png" alt="Logo" class="w-12">
-      <div>
-        <h2 class="text-xl font-bold uppercase">Best Sneakers</h2>
-        <p class="text-slate-400">Магазин лучших кроссовок</p>
+    <RouterLink to="/">
+      <div class="flex items-center gap-4">
+        <img src="/logo.png" alt="Logo" class="w-12">
+        <div>
+          <h2 class="text-xl font-bold uppercase">Best Sneakers</h2>
+          <p class="text-slate-400">Магазин лучших кроссовок</p>
+        </div>
       </div>
-    </div>
+    </RouterLink>
 
     <ul class="flex items-center gap-8">
       <li @click="() => emit('openDrawer')"
@@ -14,10 +16,12 @@
         <img src="/cart.svg" alt="Cart">
         <b>{{ totalPrice }} руб.</b>
       </li>
-      <li class="flex items-center gap-2 text-gray-500 hover:text-black cursor-pointer">
-        <img src="/heart.svg" alt="Favorites">
-        <span>Закладки</span>
-      </li>
+      <RouterLink to="/favorites">
+        <li class="flex items-center gap-2 text-gray-500 hover:text-black cursor-pointer">
+          <img src="/heart.svg" alt="Favorites">
+          <span>Закладки</span>
+        </li>
+      </RouterLink>
       <li class="flex items-center gap-2 text-gray-500 hover:text-black cursor-pointer">
         <img src="/profile.svg" alt="Profile">
         <span>Профиль</span>
