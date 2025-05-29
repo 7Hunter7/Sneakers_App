f4e7fd43b5817fc81b40159ae295883bbcd886e8
Hunter <i.am.hunter.777.ih@gmail.com>
2025-05-29 18:48:49 +0300

    upd(main.js): added use vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory} from 'vue-router'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import App from './App.vue'
+import HomePage from './pages/HomePage.vue';
+import FavoritesPage from './pages/FavoritesPage.vue';
 
 const app =  createApp(App);
-app.use(autoAnimatePlugin);
 
+const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/favorites', name: 'Favorites', component: FavoritesPage },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+app.use(autoAnimatePlugin);
+app.use(router);
 app.mount('#app');
